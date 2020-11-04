76bf42b2df098390ebe92926977e67fca9bb70c4
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-11-04 17:32:43 -0500

    Added questions

--- index.js
+++ index.js
@@ -9,8 +9,52 @@ const questions = [
         name: 'title',
         message: 'What is your title of your project?',
       },
-
-    
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe what your project does?',
+      },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install your application?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Are there any test instructions?',
+      },
+      {
+        type: 'input',
+        name: 'tech',
+        message: 'What technology did you use?',
+      },
+      {
+        type: 'input',
+        name: 'url',
+        message: 'Please share the complete URL to access your project.',
+      },
+      {
+        type: 'input',
+        name: 'contributers',
+        message: 'Who contributed to your project?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like to use?',
+        choices: ["MIT", "Apache License", "GNU AGPLv3","GPL"]
+      },
 
 ];
 
